1b7512cf3574b953331a73808f6260b912254890
vlad <vlad.charny@gmail.com>
2022-09-10 20:28:41 +0300

    edited textboxes

--- src/components/authentication/LoginForm.js
+++ src/components/authentication/LoginForm.js
@@ -1,23 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from "./LoginForm.module.css";
 
 const LoginForm = () => {
+  const [userBox, changeUserBox] = useState("");
+  const [passBox, changePassBox] = useState("");
+
+  const userChangeHandler = (event) => {
+    changeUserBox(event.target.value);
+  };
+
+  const passChangeHandler = (event) => {
+    changePassBox(event.target.value);
+  };
+
   return (
     <div className={styles.container}>
-    <form>
-      <div id={styles.userDiv}>
-        <label>Username</label>
-        <input type="text" name="" value=""/>
-      </div>
-      <div id={styles.passDiv}>
-        <label>Password</label>
-        <input type="Password" name="" value=""/>
-      </div>
-      <button type="submit" id={styles.signin} >Sign in</button>
-      <button type="button" id={styles.create} >Create Account</button>
-      <button type="button" id={styles.forgot} >Forgot Password?</button>
-      <button type="button" id={styles.guest} >Continue as a guest</button>
-    </form>
+      <form>
+        <div id={styles.userDiv}>
+          <label>Username</label>
+          <input type="text" name="" value={userBox} onChange={userChangeHandler}/>
+        </div>
+        <div id={styles.passDiv}>
+          <label>Password</label>
+          <input type="Password" name="" value={passBox} onChange={passChangeHandler}/>
+        </div>
+        <button type="submit" id={styles.signin} >Sign in</button>
+        <button type="button" id={styles.create} >Create Account</button>
+        <button type="button" id={styles.forgot} >Forgot Password?</button>
+        <button type="button" id={styles.guest} >Continue as a guest</button>
+      </form>
     </div>
   );
 };
